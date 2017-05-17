69602e40565043336954f54357fd30b94503ee0a
Eduardo Riol Fernández <eriol@atsistemas.com>
2017-05-17 16:50:45 +0200

    CartPOM movido a paquete pageobjects

--- cucumber-selenium-example/src/test/java/pageobjects/CartPOM.java
+++ cucumber-selenium-example/src/test/java/pageobjects/CartPOM.java
@@ -1,4 +1,4 @@
-package stepdefinitions;
+package pageobjects;
 
 import org.openqa.selenium.WebDriver;
 
--- cucumber-selenium-example/src/test/java/stepdefinitions/Steps.java
+++ cucumber-selenium-example/src/test/java/stepdefinitions/Steps.java
@@ -12,6 +12,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import pageobjects.AccessoriesPOM;
+import pageobjects.CartPOM;
 import pageobjects.CharactersPOM;
 import pageobjects.HomePOM;
 import pageobjects.ProductPOM;
